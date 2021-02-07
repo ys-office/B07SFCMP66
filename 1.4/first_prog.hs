b829db5994315d32cc84da649587a9a828e76f0e
Yusuke Sato <yusuke.sato@ys-office.me>
2021-02-07 23:19:40 +0900

    complate 1.4

--- 1.4/first_prog.hs
+++ 1.4/first_prog.hs
@@ -1,10 +1,16 @@
-messyMain :: IO()
-messyMain = do
+toPart recipient = "Dear " ++ recipient ++ ",\n"
+bodyPart bookTitle = "Thanks for buying " ++ bookTitle ++ ".\n"
+fromPart author = "Thanks,\n" ++ author
+
+createEmail recipient bookTitle author = toPart recipient ++
+                        bodyPart bookTitle ++
+                        fromPart author
+
+main = do
   print "Who is the email for?"
   recipient <- getLine
   print "What is the Title"
   title <- getLine
   print "Who is the Author?"
   author <- getLine
-  print("Dear " ++ recipient ++ ",\n" ++
-       "Thanks for buying " ++ title ++ "\nthanks,\n" ++ author)
+  print(createEmail recipient title author)
